32ab4a3e91dd53d4def58d994f616daadcda9e7a
abdmmar <opowae909@gmail.com>
2020-08-20 16:54:49 +0700

    � Refactor Code

---
+++ src/script/components/card-item.js
@@ -0,0 +1,26 @@
+const workContent = workData => {
+    //Create element and get element
+    const article = document.createElement(`article`);
+    const div = document.createElement(`div`);
+    const img = document.createElement(`img`);
+    const h3 = document.createElement(`h3`);
+    const small = document.createElement(`small`);
+    const work = document.querySelector(`.work`);
+
+    article.setAttribute(`tabindex`, `0`);
+    article.setAttribute(`data-id`, `${workData.id}`);
+    img.setAttribute(`src`, `../src/asset/work/${workData.id}.png`);
+    img.setAttribute(`alt`, `${workData.name}`);
+    div.classList.add(`work-detail`);
+    h3.innerHTML = workData.name;
+    small.innerHTML = `${workData.year} | ${workData.category}`;
+
+    //append attribut
+    div.append(h3);
+    div.append(small);
+    article.append(img);
+    article.append(div);
+    work.appendChild(article);
+}
+
+export {workContent};
---
+++ src/script/data/data-source.js
@@ -0,0 +1,32 @@
+const dataWorks = [
+    {
+        id: 4,
+        link: `koleksi-sinema-indonesia`,
+        name: `Koleksi Sinema Indonesia`,
+        year: `2020`,
+        category: `Web Development`
+    },
+    {
+        id: 2,
+        link: `suntree`,
+        name: `Suntree`,
+        year: `2020`,
+        category: `Brand Identity`
+    }, 
+    {
+        id: 3,
+        link: `golden-techno`,
+        name: `Golden Techno`,
+        year: `2019`,
+        category: `Brand Identity`
+    },
+    {
+        id: 1,
+        link: `university-expo`,
+        name: `University Expo 2019`,
+        year: `2019`,
+        category: `Brand Identity`
+    },
+];
+
+export {dataWorks};
